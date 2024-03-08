b429900112e4a8d515aa3b6406987027f168c6bb
Your Name <zafarbekolimboyev07@gmail.com>
2024-03-08 17:54:42 +0500

    .

---
+++ pages_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import index,uz,eng,ru
+urlpatterns = [
+    path('weekdays',index,name='main'),
+    path('weekdays/uz',uz,name='uz'),
+    path('weekdays/eng',eng,name='eng'),
+    path('weekdays/ru',ru,name='ru')
+]
